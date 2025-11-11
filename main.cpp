b76c9ece973124479cd2133e1a6feda350fff31f
Adam Amr <aamr20041989@gmail.com>
2025-11-11 09:47:06 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -17,21 +17,55 @@ this->name = name;
 this-> id = id;
    }
 
+void person::display(){
+cout<< " Name: " << this->name<<endl;
+cout << " ID: " <<this->id;
+}
+
+
+person::~person(){
+}
 
 
 
 
 
 
-// ==================== Student Class Implementation ====================
+student::student(){
+this-> yearLevel = 0;
+this-> major = "NaN";
+}
+student::student(int yearLevel , string major)::person(string name , int id){
+this->yearLevel = yearLevel;
+this->major = major;
+}
+
+void student::display(){
+person::display();
+cout << " Major: " << this-> major<<endl;;
+cout<< " Year Level " << this-> yearLever<<endl;
+}
+student::~student(){
+}
 
 
 
 
 
-
-// ==================== Instructor Class Implementation ====================
-
+istructor::instructor(){
+this-> department = "NaN";
+this-> experienceYears = 0;
+}
+instructor::instructor (string department , int experienceYears)::person(string name , int id){
+this-> department = department;
+this-> experienceYears = experienceYears;
+}
+instructor::~instructor(){
+}
+void instructor:: display(){
+person::display();
+cout<< "department" << this->department<<endl;
+cout<<" Experience Year" << this-> experienceYears<< endl;
 
 
 
